04a76c84b2af00e7344b73495273efef73d6e460
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-03-27 10:37:47 +0100

    Update HC_PROT_LIP.R

--- R/HC_PROT_LIP.R
+++ R/HC_PROT_LIP.R
@@ -1,1 +1,19 @@
-
+hc.prot.lip <- function(){
+  
+  n1<-readline(prompt="Calorias diarias: " )
+  
+  n1<-as.integer(n1)
+  
+  hc <- n1 * 0.6
+  prot <- n1 * 0.15
+  lip <- n1 * 0.25
+  lip_veg <- lip * 0.7
+  lip_an <- lip * 0.3
+  lip_sat <- n1 * 0.07
+  
+  message("Calorias Hidratos de carbono: ", hc)
+  message("Calorias Proteina: ", prot)
+  message("Calorias Grasa total: ", lip)
+  cat("Grasa vegetal: ", lip_veg, " ; Grasa animal: ", lip_an," ; Grasa saturada < ", lip_sat)
+  
+}
