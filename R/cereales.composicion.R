ac0a8cdded07d6dc1b0a8e36d688431858e4b45f
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-03-29 10:44:08 +0100

    Update cereales.composicion.R

--- R/cereales.composicion.R
+++ R/cereales.composicion.R
@@ -1,5 +1,9 @@
 cereales.composicion <- function(){
 
+  library(sqldf)
+
+  db <- dbConnect(SQLite(), dbname="Cereales")
+  
   ncer<-readline(prompt="Cereales patron de busqueda: " )
   cer <- paste('%', ncer, '%',sep = '') 
   dbGetQuery(db, "select * from Cereales where Cereales like ?", cer)
