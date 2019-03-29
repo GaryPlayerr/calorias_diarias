a25c9cf6dbb08e02bd248260b0376ab7905b85ff
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-03-29 23:17:10 +0100

    Update calorias_diarias.R

--- R/calorias_diarias.R
+++ R/calorias_diarias.R
@@ -1,12 +1,9 @@
 calorias.diarias <- function(){
-  n1<-readline(prompt="Sexo(M/F): " )
 
-  while (n1 != 'M' | n1 != 'F')
-
+  while (n1 != 'M' & n1 != 'F')
+        n1<-readline(prompt="Sexo(M/F): " )
         message('ERROR, teclea M o F') 
         
-        n1<-readline(prompt="Sexo(M/F): " )
-  
   n2<-readline(prompt="Edad: " )
   n3<-readline(prompt="Altura: " )
   n4<-readline(prompt="Peso: " )
