892f4029cb19733e4a5311e0e10a19640ec5f740
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-03-29 10:42:43 +0100

    Update cereales.composicion.R

--- R/cereales.composicion.R
+++ R/cereales.composicion.R
@@ -1,3 +1,7 @@
+cereales.composicion <- function(){
+
   ncer<-readline(prompt="Cereales patron de busqueda: " )
   cer <- paste('%', ncer, '%',sep = '') 
   dbGetQuery(db, "select * from Cereales where Cereales like ?", cer)
+
+}
