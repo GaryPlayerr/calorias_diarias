ffd8536b30e6f7deca91cc1929ce177dd9b2a96a
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-03-29 22:59:18 +0100

    Update calorias_diarias.R

--- R/calorias_diarias.R
+++ R/calorias_diarias.R
@@ -1,19 +1,19 @@
 calorias.diarias <- function(){
   n1<-readline(prompt="Sexo(M/F): " )
 
-  if  (n1 == 'M')
-    {
-    TMB = (10 * n4) + (6.25 * n3) - (5 * n2) + 5 
-    }
-  else if  (n1 == 'F')
-    {
-    TMB = (10 * n4) + (6.25 * n3) - (5 * n2) - 161   
-    }
-  else
-    {
-      message('ERROR, teclea M o F') 
-      break
-    }
+  while (n1 != 'M' & n1 != 'F')
+    if  (n1 == 'M')
+      {
+      TMB = (10 * n4) + (6.25 * n3) - (5 * n2) + 5 
+      }
+    else if  (n1 == 'F')
+      {
+      TMB = (10 * n4) + (6.25 * n3) - (5 * n2) - 161   
+      }
+    else
+      {
+        message('ERROR, teclea M o F') 
+      }
   
   n2<-readline(prompt="Edad: " )
   n3<-readline(prompt="Altura: " )
