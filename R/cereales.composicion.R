8f67f753d00f3ca60daac3bdd67913e45b5a6ff0
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-03-29 11:18:58 +0100

    Update cereales.composicion.R

--- R/cereales.composicion.R
+++ R/cereales.composicion.R
@@ -7,5 +7,9 @@ cereales.composicion <- function(){
   ncer<-readline(prompt="Cereales patron de busqueda: " )
   cer <- paste('%', ncer, '%',sep = '') 
   dbGetQuery(db, "select * from Cereales where Cereales like ?", cer)
+  
+  install.packages("DT")
+  library(DT)
+  datatable(cereales)
 
 }
