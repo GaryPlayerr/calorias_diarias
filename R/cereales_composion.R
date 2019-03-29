b0d53a6c48b53db71ecc283e6803a8f0c838b37c
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-03-29 10:05:11 +0100

    Update cereales_composion.R

--- R/cereales_composion.R
+++ R/cereales_composion.R
@@ -1,5 +1,32 @@
 cereales.composicion <- function(){
 
+  library(sqldf)
+
+  db <- dbConnect(SQLite(), dbname="Cereales")
+
+  dbSendQuery(conn = db,
+       "CREATE TABLE Cereales
+       (Cereales TEXT,
+        Estado TEXT,
+        Calorias FLOAT,
+        Proteina FLOAT,
+        Grasas FLOAT,
+        Carbohidratos FLOAT,
+        Agua FLOAT,
+        Cenizas FLOAT)")
+
+  dbListTables(db)
+  dbListFields(db, "Cereales")    
+
+  res <- dbReadTable(db, "Cereales")
+  
+  dbWriteTable(conn = db, name = "Cereales", value = cereales, append = TRUE)
+
+  dbGetQuery(db, "select * from Cereales")
+
+  dbRemoveTable(db, "Cereales")
+  dbListTables(db)
+  out <- dbWriteTable(db, "Cereales", cereales)
   ncer<-readline(prompt="Cereales patron de busqueda: " )
   cer <- paste('%', ncer, '%',sep = '') 
   dbGetQuery(db, "select * from Cereales where Cereales like ?", cer)
