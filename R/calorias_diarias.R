ede191d042afba046f32aa8b70373c205d206a14
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-03-31 23:41:01 +0200

    Update calorias_diarias.R

--- R/calorias_diarias.R
+++ R/calorias_diarias.R
@@ -86,4 +86,16 @@ calorias.diarias <- function(){
   
   print(calorias)
   
+  hc <- calorias * 0.6
+  prot <- calorias * 0.15
+  lip <- calorias * 0.25
+  lip_veg <- lip * 0.7
+  lip_an <- lip * 0.3
+  lip_sat <- calorias * 0.07
+
+  message("Calorias Hidratos de carbono: ", hc)
+  message("Calorias Proteina: ", prot)
+  message("Calorias Grasa total: ", lip)
+  cat("Grasa vegetal: ", lip_veg, " ; Grasa animal: ", lip_an," ; Grasa saturada < ", lip_sat)
+  
 }
