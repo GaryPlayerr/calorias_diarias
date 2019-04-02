8348ddf74e0429334d69974d8a82cfee8f074aa0
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-02 19:39:01 +0200

    Create insulina.R

---
+++ R/insulina.R
@@ -0,0 +1,54 @@
+insulina <- function()
+
+{
+
+ n1<-readline(prompt="Accion de la Insulina (RAPIDA o ULTRARRAPIDA): ")
+
+ library(sqldf)
+
+ if  (n1 == 'ULTRARRAPIDA')
+
+       {
+
+          db <- dbConnect(SQLite(), dbname="insulina_total")
+
+          ultra<-readline(prompt="Seleccione insulina ultrarrapida LISPRO, ASPARTA O  GLULISINA en MAYUSCULAS, espacios todas: " )
+
+          if  (ultra == ' '){}
+
+          insulina1 <- paste('%', ultra, '%',sep = '') 
+
+          ultrarrapida <- dbGetQuery(db, "select * from insulina_total where Tipo like ?", insulina1)
+          
+          library(DT)
+
+          datatable(ultrarrapida)
+       }
+       
+ else if (n1 == 'RAPIDA')    
+ 
+        {
+
+          db <- dbConnect(SQLite(), dbname="insulina_total")
+          insulina2 <- paste('RAPIDA', '%',sep = '') 
+
+          rapida <- dbGetQuery(db, "select * from insulina_total where Tipo like ?", insulina2)
+          
+          library(DT)
+
+          datatable(rapida)
+       }
+ else 
+ 
+         {
+
+          db <- dbConnect(SQLite(), dbname="insulina_total")
+
+          todas <- dbGetQuery(db, "select * from insulina_total)
+          
+          library(DT)
+
+          datatable(todas)
+       }
+ 
+ }
