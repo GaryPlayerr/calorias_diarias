2e52082f7b32e1fa07126fbfb3470f5d3d2e6393
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-02 21:14:45 +0200

    Update insulina.R

--- R/insulina.R
+++ R/insulina.R
@@ -16,7 +16,7 @@ insulina <- function()
 
           insulina1 <- paste('%', ultra, '%',sep = '')
           
-          ultrarrapida <- dbGetQuery(db, "select * from insulina_total where Tipo like ?", insulina1 )
+          ultrarrapida <- dbGetQuery(db, "select * from insulina_total where Tipo like ?", insulina1 
                                           AND Accion = 'ULTRARRAPIDA')
           
           library(DT)
