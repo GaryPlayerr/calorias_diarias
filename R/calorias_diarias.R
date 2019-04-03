687b3f91e1c1db4971cd063f7c543cc2751d8afe
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-03 17:13:59 +0200

    Update calorias_diarias.R

--- R/calorias_diarias.R
+++ R/calorias_diarias.R
@@ -176,7 +176,7 @@ calorias.diarias <- function(){
       Ratio <- 0  
       U_Insulina_Correcion <- 0 
       U_Insulina_Comida <- 0 
-      Comentarios <- ' '         
+      Comentarios <- 'PERFIL'         
     
       perfil_insulina <- data.frame(Fecha,Sexo,Edad, Altura, Peso, Distancia, Tiempo, Actividad, Calorias_Totales, Calorias_Carbohidratos, Calorias_Proteinas, Calorias_Grasa, Gramos_Carbohidratos, Dia, Horarios_Comida, Alimento, Glucemia_Real, Glucemia_Objetivo, Tipo_Insulina, FSI, Ratio, U_Insulina_Correcion, U_Insulina_Comida, Comentarios)
     
