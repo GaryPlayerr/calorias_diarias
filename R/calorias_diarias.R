fbc49cfa2b907bd3bc2e3076b8cc75dfd3c88df5
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-03 21:39:05 +0200

    Update calorias_diarias.R

--- R/calorias_diarias.R
+++ R/calorias_diarias.R
@@ -181,38 +181,11 @@ calorias.diarias <- function(){
             U_Insulina_Comida <- 0 
             Comentarios <- 'PERFIL'   
         
-            perfil_insulina <- data.frame(Fecha,Sexo,Edad, Altura, Peso, Distancia, Tiempo, Actividad, Calorias_Totales, Calorias_Carbohidratos, Calorias_Proteinas, Calorias_Grasa, Gramos_Carbohidratos, Dia, Horarios_Comida, Alimento, Glucemia_Real, Glucemia_Objetivo, Tipo_Insulina, FSI, Ratio, U_Insulina_Correcion, U_Insulina_Comida, Comentarios)
-    
             library(sqldf)
       
             db <- dbConnect(SQLite(), dbname="perfil_insulina")
-
-            dbExecute(conn = db, "INSERT INTO perfil_insulina 
-            (Fecha, 
-            Sexo, 
-            Edad, 
-            Altura, 
-            Peso, 
-            Distancia,
-            Tiempo,
- 	          Actividad ,    
-            Calorias_Totales,
-            Calorias_Carbohidratos ,
-            Calorias_Proteinas ,
-            Calorias_Grasa ,
-            Gramos_Carbohidratos ,
-            Dia ,
-            Horarios_Comida, 
-            Alimento, 
-            Glucemia_Real, 
-            Glucemia_Objetivo, 
-            Tipo_Insulina, 
-            FSI, 
-            Ratio, 
-            U_Insulina_Correcion, 
-            U_Insulina_Comida,
-            Comentarios)
-            VALUES   
+ 
+            query <- "INSERT INTO perfil_insulina VALUES   
             (Sys.Date(), 
             n1, 
             n2, 
@@ -236,11 +209,12 @@ calorias.diarias <- function(){
             0, 
             0, 
             0,
-            'PERFIL')")            
+            'PERFIL')"
+    
+            out   <- dbSendQuery(conn = db, query)
         
             dbRemoveTable(db, "perfil_insulina")
             dbListTables(db)
-            out <- dbWriteTable(db, "perfil_insulina", perfil_insulina)
         
           }
     }
