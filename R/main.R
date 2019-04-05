543ed94440da82ac8a647e297f75e6aad7ffc85f
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-05 22:16:42 +0200

    Update main.R

--- R/main.R
+++ R/main.R
@@ -11,9 +11,9 @@ print('6 - Consultar BBDD de Insulinas')
 print('7 - Ayuda')
   
 n1<-readline(prompt="Introduce una opcion: " )  
-while (n1 != 0 & n1 != 1 & n1 != 2 & n1 != 3 & n1 != 4 & n1 != 5 & n1 != 6 & n1 != 7)
+while (n1 != 0 & n1 != 1 & n1 != 2 & n1 != 3 & n1 != 4 & n1 != 5 & n1 != 6 & n1 != 7 & n1 == 'X')
 {
-  n1<-readline(prompt="Introduce una opcion valida: " ) 
+  n1<-readline(prompt="Introduce una opcion valida: (X salir) " ) 
 }  
   if  (n1 == 0)
     {  
