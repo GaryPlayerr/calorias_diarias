d59ce4f40c04b4f284dbe761cab1fcffd000a0c7
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-05 21:17:57 +0200

    Update main.R

--- R/main.R
+++ R/main.R
@@ -11,7 +11,8 @@ print('6 - Consultar BBDD de Insulinas')
 print('7 - Ayuda')
   
 n1<-readline(prompt="Introduzca una opcion: " )  
-
+while (n1 != 0 & n1 != 1 & n1 != 2 & n1 != 3 & n1 != 4 & n1 != 5 & n1 != 6 & n1 != 7)
+{  
   if  (n1 == 0)
     {  
       alimentos.composicion.create()
@@ -67,5 +68,7 @@ n1<-readline(prompt="Introduzca una opcion: " )
           {
             ?unidades.insulina 
           } 
-    }   
+    }
+ n1<-readline(prompt="Introduzca una opcion: " ) 
+ } 
 }
