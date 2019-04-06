21396b809f5f4e49dfa099b24074c0a9368b74da
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-06 22:53:10 +0200

    Update calorias_diarias.R

--- R/calorias_diarias.R
+++ R/calorias_diarias.R
@@ -233,20 +233,7 @@ calorias.diarias <- function(){
   
     if  (tabla1 == 'S' | tabla1 == 's')
       {
-        library("readxl")
-    
-        n8<-readline(prompt="Tiempo(minutos) : " )
-        n8<-as.integer(n8)
-        tabla1 <- 'S'
-        url1<-'https://github.com/BorjaJorge/calorias_diarias/raw/master/R/MET.xlsx'
-        p1f <- tempfile()
-        download.file(url1, p1f, mode="wb")
-        tabla<-read_excel(path = p1f)
-        tabla$Calorias_que_quemarias <- tabla$Calorias * 0.0175 * n4 * n8
-        tabla$Calorias <- NULL
-        library(DT)
-        datatable(tabla)
-    
+        otros.deportes()  
       }
     else if (tabla1 == 'N' | tabla1 == 'n' | tabla1 =='')
       {
