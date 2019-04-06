ec9fdb8d12a98e8e5e0f01716e99348b49deb68e
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-06 22:52:20 +0200

    Update otros_deportes.R

--- R/otros_deportes.R
+++ R/otros_deportes.R
@@ -1,3 +1,16 @@
 
 otros.deportes <- function(){
+        library("readxl")
+    
+        n8<-readline(prompt="Tiempo(minutos) : " )
+        n8<-as.integer(n8)
+        tabla1 <- 'S'
+        url1<-'https://github.com/BorjaJorge/calorias_diarias/raw/master/R/MET.xlsx'
+        p1f <- tempfile()
+        download.file(url1, p1f, mode="wb")
+        tabla<-read_excel(path = p1f)
+        tabla$Calorias_que_quemarias <- tabla$Calorias * 0.0175 * n4 * n8
+        tabla$Calorias <- NULL
+        library(DT)
+        datatable(tabla)
 }
