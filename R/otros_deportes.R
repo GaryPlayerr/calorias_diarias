9e515834ed813d4767cc962026fc675fa7838672
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-06 22:56:34 +0200

    Update otros_deportes.R

--- R/otros_deportes.R
+++ R/otros_deportes.R
@@ -3,6 +3,7 @@ otros.deportes <- function(){
         library("readxl")
     
         n8<-readline(prompt="Tiempo(minutos) : " )
+        n4<-readline(prompt="Peso(Kg) : " )
         n8<-as.integer(n8)
         tabla1 <- 'S'
         url1<-'https://github.com/BorjaJorge/calorias_diarias/raw/master/R/MET.xlsx'
