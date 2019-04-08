6f864bfb2f43257b65f5a16d21e210c3c8866ebd
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-08 10:46:44 +0200

    Update main.R

--- R/main.R
+++ R/main.R
@@ -13,7 +13,7 @@ print('8 - Recomendaciones Asociacion Americana de Diabeticos')
 print('9 - Ayuda')
   
 n1<-readline(prompt="Introduce una opcion: " )  
-while (n1 != 0 & n1 != 1 & n1 != 2 & n1 != 3 & n1 != 4 & n1 != 5 & n1 != 6 & n1 != 7)
+while (n1 != 0 & n1 != 1 & n1 != 2 & n1 != 3 & n1 != 4 & n1 != 5 & n1 != 6 & n1 != 7 & n1 != 8 & n1 != 9)
 {
   n1<-readline(prompt="Introduce una opcion valida: " ) 
 }  
