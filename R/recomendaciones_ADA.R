c0212773ce3c01d6cbbb0fa783900205ef8c5ccc
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-08 10:40:13 +0200

    Update recomendaciones_ADA.R

--- R/recomendaciones_ADA.R
+++ R/recomendaciones_ADA.R
@@ -1,18 +1,21 @@
 recomendaciones.ADA <- function(){
 
-  library(DT)
-  install.packages("rvest")
-  library(rvest)
-  library(stringr)
+library(DT)
+install.packages("rvest")
+library(rvest)
+library(stringr)
 
-recomendaciones_ADA<- read_html("http://www.diabetes.org/es/vivir-con-diabetes/tratamiento-y-cuidado/el-control-de-la-glucosa-en-la-sangre/cmo-medir-la-glucosa-en-la.html") 
+path <- 'http://www.diabetes.org/es/vivir-con-diabetes/tratamiento-y-cuidado/el-control-de-la-glucosa-en-la-sangre/cmo-medir-la-glucosa-en-la.html'
+recomendaciones_ADA<- read_html(path) 
 
 rec_ADA <-
   recomendaciones_ADA %>%
   html_nodes(css = "section.content-view-full") %>%
   html_text()
-  
+
 ADA <- str_split(rec_ADA, '\n')
-ADA <- as.data.frame(ADA)
-ADA
+ADA <- data.frame(ADA)
+ADA <- ADA[8:16,]
+dataf <- data.frame(Recomendaciones_Asociacion_Americana_Diabeticos =  ADA)
+datatable(dataf)
 }  
