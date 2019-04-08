3e2dd02febf206fd6bc753dac3a33903aff65be9
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-08 10:46:08 +0200

    Update main.R

--- R/main.R
+++ R/main.R
@@ -10,7 +10,7 @@ print('5 - Consultar BBDD de alimentos:')
 print('6 - Consultar BBDD de Insulinas')  
 print('7 - Calorias estimadas varias actividades fisicas')  
 print('8 - Recomendaciones Asociacion Americana de Diabeticos')  
-print('8 - Ayuda')
+print('9 - Ayuda')
   
 n1<-readline(prompt="Introduce una opcion: " )  
 while (n1 != 0 & n1 != 1 & n1 != 2 & n1 != 3 & n1 != 4 & n1 != 5 & n1 != 6 & n1 != 7)
