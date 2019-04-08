fb9d3a17a96d59f0c0fa17ce76a225806c81c6b9
BorjaJorge <48928500+BorjaJorge@users.noreply.github.com>
2019-04-08 10:59:41 +0200

    Update recomendaciones_ADA.R

--- R/recomendaciones_ADA.R
+++ R/recomendaciones_ADA.R
@@ -18,5 +18,12 @@ ADA <- data.frame(ADA)
 ADA <- ADA[8:16,]
 dataf <- data.frame(Recomendaciones_Asociacion_Americana_Diabeticos =  ADA)
 datatable(dataf, class = 'cell-border stripe', rownames = FALSE, 
-          caption = 'Recomendaciones ADA.', filter = 'top')
+          caption = 'Recomendaciones ADA.', filter = 'top',options = list(
+  initComplete = JS("
+    function(settings, json) {
+      $(this.api().table().header()).css({
+        'background-color': '#000',
+        'color': '#fff'
+      });
+    }"))
 }  
